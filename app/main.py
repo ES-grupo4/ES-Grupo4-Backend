5e21ccbe178c1c81ef1e2787c935f656df8228fb
Pittersss <pedromalaquias608@gmail.com>
2025-07-12 16:03:51 -0300

    feat: fill database with csv file

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from app.routers import employees  # noqa: F401
 
 app = FastAPI()
 
+app.include_router(employees.router)
 
 # Sempre preencham summary e tags
 # Tags fazem sentido de ir nos APIRouter tmb
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+]
+
+
 @app.get("/", summary="Te dá oi", tags=["Tag foda"])
 def root():
     return {"message": "Hello world"}
