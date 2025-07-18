5412d5888abe4d1acc43852b4bf7101cadaa78eb
aelsonjrdiniz <aelson.junior.araujo.diniz.cunha@ccc.ufcg.edu.br>
2025-07-18 13:52:29 -0300

    fix(models): Revisando e corrigindo entidades

--- app/models/models.py
+++ app/models/models.py
@@ -1,84 +1,76 @@
-from typing import List
-from typing import Optional
 from datetime import datetime
-from sqlalchemy import ForeignKey
-from sqlalchemy import String
-from sqlalchemy import DateTime
-from sqlalchemy import JSON
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
-from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey, String, DateTime, JSON
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 class Base(DeclarativeBase):
-    pass
+  pass
 
 class Usuario(Base):
-    __tablename__ = "usuarios"
+  __tablename__ = "usuario"
 
-    cpf: Mapped[str] 
-    nome: Mapped[str]
-    id: Mapped[int] = mapped_column(primary_key=True)
-    tipo: Mapped[str] = mapped_column(String)
+  cpf: Mapped[str | None] = mapped_column(String(11))
+  nome: Mapped[str | None]
+  id: Mapped[int] = mapped_column(primary_key=True)
+  subtipo: Mapped[str] = mapped_column(String())
 
-    __mapper_args__ = {
-        "polymorphic_identity": "usuarios",
-        "polymorphic_on": tipo,
-    }
+  __mapper_args__ = {
+    "polymorphic_identity": "usuario",
+    "polymorphic_on": subtipo,
+  }
 
 class Funcionario(Usuario):
-    __tablename__ = "funcionarios"
+  __tablename__ = "funcionario"
 
-    usuario_id: Mapped[int] = mapped_column(ForeignKey(Usuario.id), primary_key=True)
-    tipo: Mapped[str]
-    senha: Mapped[str]
-    email: Mapped[str]
+  usuario_id: Mapped[int] = mapped_column(ForeignKey(Usuario.id), primary_key=True)
+  subtipo: Mapped[str]
+  senha: Mapped[str]
+  email: Mapped[str | None]
     
-    __mapper_args__ = {
-        "polymorphic_identity": "funcionarios",
-    }
+  __mapper_args__ = {
+    "polymorphic_identity": "funcionario",
+  }
 
 class Cliente(Usuario):
-    __tablename__ = "clientes"
+  __tablename__ = "cliente"
 
-    usuario_id: Mapped[int] = mapped_column(ForeignKey(Usuario.id), primary_key=True)
-    matricula: Mapped[str]
-    tipo: Mapped[str]
-    graduando: Mapped[bool]
-    pos_graduando: Mapped[bool]
-    bolsista: Mapped[bool]
+  usuario_id: Mapped[int] = mapped_column(ForeignKey(Usuario.id), primary_key=True)
+  matricula: Mapped[str | None] 
+  subtipo: Mapped[str]
+  graduando: Mapped[bool]
+  pos_graduando: Mapped[bool]
+  bolsista: Mapped[bool]
 
-    __mapper_args__ = {
-        "polymorphic_identity": "clientes",
-    }
+  __mapper_args__ = {
+    "polymorphic_identity": "cliente",
+  }
 
-class Informacoes_Gerais(Base):
-    __tablename__ = "informacoes_gerais"
+class InformacoesGerais(Base):
+  __tablename__ = "informacoes_gerais"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    nome_empresa: Mapped[str]
-    preco_almoco: Mapped[int]
-    preco_meia_almoco: Mapped[int]
-    preco_jantar: Mapped[int]
-    preco_meia_jantar: Mapped[int]
-    periodo_almoco: Mapped[datetime] = mapped_column(DateTime)
-    periodo_jantar: Mapped[datetime] = mapped_column(DateTime)
+  id: Mapped[int] = mapped_column(primary_key=True)
+  nome_empresa: Mapped[str]
+  preco_almoco: Mapped[int]
+  preco_meia_almoco: Mapped[int]
+  preco_jantar: Mapped[int]
+  preco_meia_jantar: Mapped[int]
+  periodo_almoco: Mapped[datetime] = mapped_column(DateTime)
+  periodo_jantar: Mapped[datetime] = mapped_column(DateTime)
 
-class Historico_Acoes(Base):
-    __tablename__ = "historico_acoes"
+class HistoricoAcoes(Base):
+  __tablename__ = "historico_acoes"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    usuario_id_ator: Mapped[int] = mapped_column(ForeignKey(Funcionario.usuario_id))
-    usuario_id_alvo: Mapped[int] = mapped_column(ForeignKey(Cliente.usuario_id))
-    informacoes_acao: Mapped[dict] = mapped_column(JSON)
+  id: Mapped[int] = mapped_column(primary_key=True)
+  usuario_id_ator: Mapped[int] = mapped_column(ForeignKey(Funcionario.usuario_id))
+  usuario_id_alvo: Mapped[int] = mapped_column(ForeignKey(Cliente.usuario_id))
+  informacoes_acao: Mapped[dict] = mapped_column(JSON)
 
 
 class Compra(Base):
-    __tablename__ = "compras"
+  __tablename__ = "compra"
 
-    usuario_id: Mapped[str] = mapped_column(primary_key=True)
-    local: Mapped[str] 
-    forma_pagamento: Mapped[str]
-    tipo_cliente: Mapped[str]
+  usuario_id: Mapped[int] = mapped_column(primary_key=True)
+  local: Mapped[str] 
+  forma_pagamento: Mapped[str]
+  tipo_cliente: Mapped[str]
 
 
