afa8b9b2632c37b8f62cb5fc541236605aacf352
Pittersss <pedromalaquias608@gmail.com>
2025-07-19 11:49:08 -0300

    fix: pull with develop and name adjust

--- app/main.py
+++ app/main.py
@@ -5,21 +5,4 @@ from .routers.funcionario import funcionarios_router
 
 
 app = FastAPI()
-<<<<<<< HEAD
-
-app.include_router(employees.router)
-
-# Sempre preencham summary e tags
-# Tags fazem sentido de ir nos APIRouter tmb
-origins = [
-    "http://localhost",
-    "http://localhost:8000",
-]
-
-
-@app.get("/", summary="Te dá oi", tags=["Tag foda"])
-def root():
-    return {"message": "Hello world"}
-=======
 app.include_router(funcionarios_router)
->>>>>>> 10ac26230d211b7d7f2a76c001ee9bf305b6c26b
