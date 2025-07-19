e01e9db30775a4d8563a581d3be0fdedbb9b1ece
joao-pedro-angelo <carneiroangelojoaopedro@gmail.com>
2025-07-19 13:38:03 -0300

    feat(cliente): adiciona rota de cliente no main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
 from .routers.funcionario import funcionarios_router
+from .routers.cliente import cliente_router
 
 
 app = FastAPI()
 app.include_router(funcionarios_router)
+app.include_router(cliente_router)
