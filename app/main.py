739c4b8ac68722aa42a7c88917f3462149349b3e
pedroln <pedro.lobo.nascimento@ccc.ufcg.edu.br>
2025-07-20 23:31:07 -0300

    feat: adiciona import do auth

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
-
 from .routers.funcionario import funcionarios_router
+from .routers.auth import auth_router
 
 
 app = FastAPI()
+
 app.include_router(funcionarios_router)
+app.include_router(auth_router)
