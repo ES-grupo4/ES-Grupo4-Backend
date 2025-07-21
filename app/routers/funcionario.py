b4af60edb8b6751efa8944532c492850bc03ff09
pedroln <pedro.lobo.nascimento@ccc.ufcg.edu.br>
2025-07-21 18:32:40 -0300

    feat(funcionario): implementa post de funcionario

--- app/routers/funcionario.py
+++ app/routers/funcionario.py
@@ -1,10 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from ..models.models import Funcionario
 from ..models.db_setup import conexao_bd
 from ..schemas.funcionario import FuncionarioOut, FuncionarioIn
+from ..utils.permissoes import requer_permissao
 
 from sqlalchemy import select
+from validate_docbr import CPF
+
+cpf = CPF()
 
 funcionarios_router = APIRouter(prefix="/funcionario", tags=["Funcionário"])
 router = funcionarios_router
@@ -12,25 +16,54 @@ router = funcionarios_router
 
 # Sempre preencham summary e tags
 # Tags fazem sentido de ir nos APIRouter tmb
+# TODO: Rota de criar funcionário:
+#   - Verificar se o funcionário existe antes de tudo
+#   - Se existir solta um erro, se não, insere
 @router.post(
-    "/{cpf}/{nome}",
-    summary="Cria um funcionário",
+    "/",
+    summary="Cria um funcionário no sistema",
+    dependencies=[requer_permissao("admin")],
 )
 def funcionario(funcionario: FuncionarioIn, db: conexao_bd):
+    if funcionario.cpf in db.scalars(select(Funcionario.cpf)):
+        raise HTTPException(status_code=409, detail="CPF já cadastrado no sistema")
+
+    if not cpf.validate(funcionario.cpf):
+        raise HTTPException(status_code=400, detail="CPF inválido")
+
+    if funcionario.email in db.scalars(select(Funcionario.email)):
+        raise HTTPException(status_code=409, detail="Email já cadastrado no sistema")
+
+    if funcionario.tipo.lower() not in ("admin", "funcionario", "funcionário"):
+        raise HTTPException(status_code=400, detail="Tipo de funcionário inválido")
+
+    if funcionario.tipo.lower() == "funcionário":
+        funcionario.tipo = "funcionario"
+
+    # NOTE: lembrar de ver como vai funcionar a aplicação da LGPD nos dados da galera: hash de cpf, senha, etc
     usuario = Funcionario(
         cpf=funcionario.cpf,
         nome=funcionario.nome,
         senha=funcionario.senha,
         email=funcionario.email,
-        tipo=funcionario.tipo,
+        tipo=funcionario.tipo.lower(),
     )
+
     db.add(usuario)
-    return {"message": "Funcionário criado com sucesso"}
+    return {"message": "Funcionário cadastrado com sucesso"}
 
-
+"""
+TODO:
+ - POST/funcionarios - Criar novo funcionário (admin route)
+ - PUT/funcionarios/{id} - Atualizar dados do funcionário (admin route)
+ - GET/funcionarios?{optional-filter}= - Buscar funcionários
+ - POST/funcionarios/credentials - Verificar credenciais de login
+ - DELETE/funcionarios/{id} - Remoção conforme LGPD (admin route)
+"""
+# NOTE: ver a questão dos funcionários ativos: vão ser listados ou não?
 @router.get(
     "/",
-    summary="Pega todos os funcionários",
+    summary="Retorna todos os funcionários cadastrados",
     tags=["Funcionário"],
     response_model=list[FuncionarioOut],
 )
