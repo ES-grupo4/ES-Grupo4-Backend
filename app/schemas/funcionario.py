c183ed009f27e7b4a66516ca6c19d9d29c59dcd2
pedroln <pedro.lobo.nascimento@ccc.ufcg.edu.br>
2025-07-27 22:12:04 -0300

    feat(funcionario): adiciona enum para tipo e validação de email com EmailStr

--- app/schemas/funcionario.py
+++ app/schemas/funcionario.py
@@ -1,26 +1,48 @@
-from pydantic import BaseModel, ConfigDict
+from enum import Enum
+from pydantic import BaseModel, ConfigDict, EmailStr
 
+
+class tipoFuncionarioEnum(str, Enum):
+    funcionario = "funcionario"
+    admin = "admin"
 
 class FuncionarioOut(BaseModel):
     id: int
     nome: str
     cpf: str
-    email: str
-    tipo: str
+    email: EmailStr
+    tipo: tipoFuncionarioEnum
 
 
 class FuncionarioIn(BaseModel):
     cpf: str
     nome: str
     senha: str
-    email: str
-    tipo: str
+    email: EmailStr
+    tipo: tipoFuncionarioEnum
 
     model_config = ConfigDict(
         json_schema_extra={
             "example": {
                 "cpf": "198.965.074-06",
                 "nome": "John Doe",
+                "senha": "John123!",
+                "email": "john@doe.com",
+                "tipo": "admin",
+            }
+        }
+    )
+
+class FuncionarioEdit(BaseModel):
+    nome: str
+    senha: str
+    email: EmailStr
+    tipo: tipoFuncionarioEnum
+
+    model_config = ConfigDict(
+        json_schema_extra={
+            "example": {
+                "nome": "John Dois",
                 "senha": "John123!",
                 "email": "john@doe.com",
                 "tipo": "admin",
