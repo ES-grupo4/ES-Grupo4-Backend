f06f2d2856c5f68169016fc0ba396e1396f02820
pedroln <pedro.lobo.nascimento@ccc.ufcg.edu.br>
2025-07-27 22:10:57 -0300

    feat(funcionario): implementa rota de delete, adiciona filtros no get e corrige verificação de funcionario

--- app/routers/funcionario.py
+++ app/routers/funcionario.py
@@ -1,13 +1,18 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Query
 
 from ..models.models import Funcionario
 from ..models.db_setup import conexao_bd
-from ..schemas.funcionario import FuncionarioOut, FuncionarioIn
+from ..schemas.funcionario import (
+    FuncionarioEdit,
+    FuncionarioOut,
+    FuncionarioIn,
+    tipoFuncionarioEnum,
+)
 from ..utils.permissoes import requer_permissao
+from pydantic import EmailStr
 
 from sqlalchemy import select
 from validate_docbr import CPF
-from email_validator import validate_email, EmailNotValidError
 
 cpf = CPF()
 
@@ -15,13 +20,6 @@ funcionarios_router = APIRouter(prefix="/funcionario", tags=["Funcionário"])
 router = funcionarios_router
 
 
-# Sempre preencham summary e tags
-# Tags fazem sentido de ir nos APIRouter tmb
-# TODO: Rota de criar funcionário:
-#   - Verificar se o funcionário existe antes de tudo
-#   - Se existir solta um erro, se não, insere
-
-# NOTE: colocar verificação se é post ou put, se for post verifica se o email e o cpf ja foram cadastrados
 def valida_funcionario(funcionario: FuncionarioIn, db: conexao_bd):
     funcionario.cpf = funcionario.cpf.replace(".", "").replace("-", "")
 
@@ -31,17 +29,8 @@ def valida_funcionario(funcionario: FuncionarioIn, db: conexao_bd):
     if funcionario.cpf in db.scalars(select(Funcionario.cpf)):
         raise HTTPException(status_code=409, detail="CPF já cadastrado no sistema")
 
-    if not validate_email(funcionario.email):
-        raise HTTPException(status_code=400, detail="Email inválido")
-
     if funcionario.email in db.scalars(select(Funcionario.email)):
         raise HTTPException(status_code=409, detail="Email já cadastrado no sistema")
-    
-    if funcionario.tipo.lower() not in ("admin", "funcionario", "funcionário"):
-        raise HTTPException(status_code=400, detail="Tipo de funcionário inválido")
-
-    if funcionario.tipo.lower() == "funcionário":
-        funcionario.tipo = "funcionario"
 
     return funcionario
 
@@ -49,12 +38,12 @@ def valida_funcionario(funcionario: FuncionarioIn, db: conexao_bd):
 @router.post(
     "/",
     summary="Cria um funcionário no sistema",
-    # dependencies=[requer_permissao("admin")],
+    tags=["Funcionário"],
+    dependencies=[requer_permissao("admin")],
 )
 def cadastra_funcionario(funcionario: FuncionarioIn, db: conexao_bd):
     funcionario = valida_funcionario(funcionario, db)
 
-    # NOTE: fazer hash de cpf, senha e
     usuario = Funcionario(
         cpf=funcionario.cpf,
         nome=funcionario.nome,
@@ -67,17 +56,7 @@ def cadastra_funcionario(funcionario: FuncionarioIn, db: conexao_bd):
     return {"message": "Funcionário cadastrado com sucesso"}
 
 
-"""
-TODO:
- - POST/funcionarios - Criar novo funcionário (admin route)
- - PUT/funcionarios/{id} - Atualizar dados do funcionário (admin route)
- - GET/funcionarios?{optional-filter}= - Buscar funcionários
- - POST/funcionarios/credentials - Verificar credenciais de login
- - DELETE/funcionarios/{id} - Remoção conforme LGPD (admin route)
-"""
-
-
-@router.put(
+@router.patch(
     "/{id}/",
     response_model=FuncionarioOut,
     summary="Atualiza os dados de um funcionário",
@@ -85,33 +64,74 @@ TODO:
     dependencies=[requer_permissao("admin")],
 )
 def atualiza_funcionario(
-    funcionario_id: int, funcionario: FuncionarioIn, db: conexao_bd
+    funcionario_id: int, funcionario: FuncionarioEdit, db: conexao_bd
 ):
     funcionarioExistente = db.scalar(
         select(Funcionario).where(Funcionario.id == funcionario_id)
     )
+
     if not funcionarioExistente:
-        raise HTTPException(status_code=404, detail="Usuário não encontrado")
+        raise HTTPException(status_code=404, detail="Funcionário não encontrado")
 
-    if not cpf.validate(funcionario.cpf):
-        raise HTTPException(status_code=400, detail="CPF inválido")
-
-    update_dict = funcionario.model_dump(exclude_unset=True)
-    for key, value in update_dict.items():
-        setattr(funcionarioExistente, key, value)
+    for campo, valor in funcionario.model_dump(exclude_unset=True).items():
+        setattr(funcionarioExistente, campo, valor)
 
     db.commit()
     db.refresh(funcionarioExistente)
     return funcionarioExistente
 
 
-# NOTE: ver a questão dos funcionários ativos: vão ser listados ou não?
 @router.get(
     "/",
     summary="Retorna todos os funcionários cadastrados",
     tags=["Funcionário"],
     response_model=list[FuncionarioOut],
 )
-def funcionarios(db: conexao_bd):
-    usuarios = db.scalars(select(Funcionario)).all()
+def busca_funcionarios(
+    db: conexao_bd,
+    id: int | None = Query(None, description="Filtra pelo id do funcionário"),
+    cpf: str | None = Query(None, description="Filtra pelo cpf do funcionário"),
+    nome: str | None = Query(None, description="Filtra pelo nome do funcionário"),
+    tipo: tipoFuncionarioEnum | None = Query(
+        None, description="Filtra pelo tipo do funcionário"
+    ),
+    email: EmailStr | None = Query(
+        None, description="Filtra pelo email do funcionário"
+    ),
+):
+    query = select(Funcionario)
+
+    if id:
+        query = query.where(Funcionario.id == id)
+
+    if cpf:
+        query = query.where(Funcionario.cpf == cpf)
+
+    if nome:
+        query = query.where(Funcionario.nome.ilike(f"%{nome}"))
+
+    if tipo:
+        query = query.where(Funcionario.tipo == tipo)
+
+    if email:
+        query = query.where(Funcionario.email == tipo)
+
+    usuarios = db.scalars(query).all()
     return usuarios
+
+
+@router.delete(
+    "/",
+    summary="Remove um funcionário pelo CPF",
+    tags=["Funcionário"],
+    dependencies=[requer_permissao("admin")],
+)
+def deleta_funcionario(db: conexao_bd, cpf: str):
+    cpf = cpf.replace(".", "").replace("-", "")
+    funcionario = db.scalar(select(Funcionario).where(Funcionario.cpf == cpf))
+    if not funcionario:
+        raise HTTPException(status_code=404, detail="Funcionário não encontrado")
+
+    db.delete(funcionario)
+    db.commit()
+    return {"message": "Funcionário deletado com sucesso"}
