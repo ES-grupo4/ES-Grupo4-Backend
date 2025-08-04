b160da36df9fe3a0d608dfd8412e4d5514858d2b
aelsonjrdiniz <aelson.junior.araujo.diniz.cunha@ccc.ufcg.edu.br>
2025-08-03 23:43:12 -0300

    feat(models): Adicionando restrições novas às classes e novos atributos

--- app/models/models.py
+++ app/models/models.py
@@ -1,5 +1,15 @@
-from datetime import datetime
-from sqlalchemy import ForeignKey, String, DateTime, JSON
+from datetime import datetime, time, date
+from sqlalchemy import (
+    ForeignKey,
+    String,
+    DateTime,
+    JSON,
+    Time,
+    Date,
+    Boolean,
+    Integer,
+    CHAR,
+)
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
@@ -10,14 +20,14 @@ class Base(DeclarativeBase):
 class Usuario(Base):
     __tablename__ = "usuario"
 
-    cpf: Mapped[str | None] = mapped_column(String(11), unique=True)
-    nome: Mapped[str | None]
+    cpf: Mapped[str | None] = mapped_column(CHAR(11), unique=True, nullable=True)
+    nome: Mapped[str | None] = mapped_column(String(100), nullable=True)
     id: Mapped[int] = mapped_column(primary_key=True)
     subtipo: Mapped[str] = mapped_column(String())
 
     __mapper_args__ = {
-        "polymorphic_identity": "usuario",
         "polymorphic_on": subtipo,
+        "polymorphic_abstract": True,
     }
 
     def __repr__(self) -> str:
@@ -28,9 +38,11 @@ class Funcionario(Usuario):
     __tablename__ = "funcionario"
 
     usuario_id: Mapped[int] = mapped_column(ForeignKey(Usuario.id), primary_key=True)
-    tipo: Mapped[str]
-    senha: Mapped[str]
-    email: Mapped[str | None]
+    tipo: Mapped[str] = mapped_column(String(50))
+    senha: Mapped[str] = mapped_column(String(50))
+    email: Mapped[str | None] = mapped_column(String(320), nullable=True)
+    data_entrada: Mapped[date] = mapped_column(Date)
+    data_saida: Mapped[date | None] = mapped_column(Date, nullable=True)
 
     __mapper_args__ = {
         "polymorphic_identity": "funcionario",
@@ -41,11 +53,11 @@ class Cliente(Usuario):
     __tablename__ = "cliente"
 
     usuario_id: Mapped[int] = mapped_column(ForeignKey(Usuario.id), primary_key=True)
-    matricula: Mapped[str | None]
-    tipo: Mapped[str]
-    graduando: Mapped[bool]
-    pos_graduando: Mapped[bool]
-    bolsista: Mapped[bool]
+    matricula: Mapped[str | None] = mapped_column(String(50), nullable=True)
+    tipo: Mapped[str] = mapped_column(String(50))
+    graduando: Mapped[bool] = mapped_column(Boolean)
+    pos_graduando: Mapped[bool] = mapped_column(Boolean)
+    bolsista: Mapped[bool] = mapped_column(Boolean)
 
     __mapper_args__ = {
         "polymorphic_identity": "cliente",
@@ -56,13 +68,15 @@ class InformacoesGerais(Base):
     __tablename__ = "informacoes_gerais"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    nome_empresa: Mapped[str]
-    preco_almoco: Mapped[int]
-    preco_meia_almoco: Mapped[int]
-    preco_jantar: Mapped[int]
-    preco_meia_jantar: Mapped[int]
-    periodo_almoco: Mapped[datetime] = mapped_column(DateTime)
-    periodo_jantar: Mapped[datetime] = mapped_column(DateTime)
+    nome_empresa: Mapped[str] = mapped_column(String())
+    preco_almoco: Mapped[int] = mapped_column(Integer)
+    preco_meia_almoco: Mapped[int] = mapped_column(Integer)
+    preco_jantar: Mapped[int] = mapped_column(Integer)
+    preco_meia_jantar: Mapped[int] = mapped_column(Integer)
+    comeco_jantar: Mapped[time] = mapped_column(Time)
+    comeco_almoco: Mapped[time] = mapped_column(Time)
+    fim_jantar: Mapped[time] = mapped_column(Time)
+    fim_almoco: Mapped[time] = mapped_column(Time)
 
 
 class HistoricoAcoes(Base):
@@ -72,12 +86,13 @@ class HistoricoAcoes(Base):
     usuario_id_ator: Mapped[int] = mapped_column(ForeignKey(Funcionario.usuario_id))
     usuario_id_alvo: Mapped[int] = mapped_column(ForeignKey(Cliente.usuario_id))
     informacoes_acao: Mapped[dict] = mapped_column(JSON)
+    data: Mapped[datetime] = mapped_column(DateTime)
 
 
 class Compra(Base):
     __tablename__ = "compra"
 
-    usuario_id: Mapped[int] = mapped_column(primary_key=True)
-    local: Mapped[str]
-    forma_pagamento: Mapped[str]
-    tipo_cliente: Mapped[str]
+    usuario_id: Mapped[int] = mapped_column(ForeignKey(Usuario.id), primary_key=True)
+    local: Mapped[str] = mapped_column(String())
+    forma_pagamento: Mapped[str] = mapped_column(String(50))
+    horario: Mapped[datetime] = mapped_column(DateTime, primary_key=True)
